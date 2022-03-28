55709e298dc06296fdd7f7a48cc39abd8ee0b85b
datvn21 <dinhtiendat0201@gmail.com>
2022-03-28 13:29:21 +0700

    Okiiila

--- pages/chap.js
+++ pages/chap.js
@@ -3,25 +3,25 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { HorizontalScroll } from "../components/HorizontalScroll";
+import Layout from "../components/Layout";
 const chap = ({ images }) => {
   const scrollRef = HorizontalScroll();
   return (
-    <div
-      ref={scrollRef}
-      className="md:h-screen h-screen w-full md:overflow-y-hidden bg-slate-200 md:w-screen flex flex-col md:flex-row overflow-x-hidden md:overflow-x-scroll"
-    >
-      {images.data?.map((im) => (
-        <img
-          className="mx-1 h-screen w-full md:w-auto"
-          key={im}
-          src={im}
-          alt={im}
-        />
-      ))}
-      <Head>
-        <title>{images.name}</title>
-      </Head>
-    </div>
+    <Layout title={images.name}>
+      <div
+        ref={scrollRef}
+        className="md:h-screen h-screen w-full md:overflow-y-hidden bg-slate-200 md:w-screen flex flex-col md:flex-row overflow-x-hidden md:overflow-x-scroll"
+      >
+        {images.data?.map((im) => (
+          <img
+            className="mx-1 h-screen w-full md:w-auto"
+            key={im}
+            src={im}
+            alt={im}
+          />
+        ))}
+      </div>
+    </Layout>
   );
 };
 export const getServerSideProps = async (context) => {
